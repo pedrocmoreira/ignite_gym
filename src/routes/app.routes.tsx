e51ff03b8eb0113c9a155d43d85f8d45af886ad5
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-14 06:37:45 -0300

    chore: adicionando icons ao tabBar

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,30 +1,59 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { useTheme } from "native-base";
+
+import HomeSvg from '@assets/home.svg';
+import HistorySvg from '@assets/history.svg';
+import ProfileSvg from '@assets/profile.svg';
 
 import { Home } from "@screens/Home";
 import { History } from "@screens/History";
 import { Profile } from "@screens/Profile";
 import { Exercise } from "@screens/Exercise";
 
-const {Navigator, Screen} = createBottomTabNavigator();
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  history: undefined;
+  profile: undefined;
+}
 
-export function AppRoutes(){
-  return(
-    <Navigator>
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
+export function AppRoutes() {
+  const { sizes } = useTheme();
+
+  const iconSizes = sizes[6]
+
+  return (
+    <Navigator screenOptions={{ headerShown:false, tabBarShowLabel: false }}>
       <Screen
         name="home"
         component={Home}
+        options={{
+          tabBarIcon: ({color}) => (
+            <HomeSvg fill={color} width={iconSizes} height={iconSizes}/>
+          )
+        }}
       />
       <Screen
         name="history"
         component={History}
-      />
-      <Screen
-        name="history"
-        component={History}
+        options={{
+          tabBarIcon: ({color}) => (
+            <HistorySvg fill={color} width={iconSizes} height={iconSizes}/>
+          )
+        }}
       />
       <Screen
         name="profile"
         component={Profile}
+        options={{
+          tabBarIcon: ({color}) => (
+            <ProfileSvg fill={color} width={iconSizes} height={iconSizes}/>
+          )
+        }}
       />
       <Screen
         name="exercise"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { useTheme, Box } from 'native-base';
+import { AppRoutes } from './app.routes';
 
 import { AuthRoutes } from './auth.routes'
 
@@ -13,7 +14,7 @@ export function Routes() {
   return (
     <Box flex={1} bg='gray.700'>
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )
