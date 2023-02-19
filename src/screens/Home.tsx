f13b4be9b2f3cbcddde1bf50254abf6c982f0b16
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-02-19 15:04:55 -0300

    chore: adicionando grupos de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,45 @@
-import { Center, Text, VStack } from 'native-base';
+import { HStack, VStack, FlatList, Heading, Text } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
+import { Group } from '@components/Group';
+import { useState } from 'react';
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro']);
+  const [groupSelected, setGroupSelected] = useState('costa');
+
   return (
     <VStack flex={1}>
-      <HomeHeader/>
+      <HomeHeader />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent='space-between'>
+          <Heading color='gray.200' fontSize='md'>
+            Exercícios
+          </Heading>
+
+          <Text color='gray.200' fontSize='sm'>
+            4
+          </Text>
+        </HStack>
+      </VStack>
+
     </VStack>
   )
 }
