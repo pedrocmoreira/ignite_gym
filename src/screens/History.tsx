434920ac90c2a6d07d12548709af6760922ed33b
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-04-02 11:12:44 -0300

    chore: criando componente de historyCard

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,14 @@
-import { VStack, Text } from 'native-base';
+import { VStack } from 'native-base';
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
   return (
     <VStack flex={1}>
-      <ScreenHeader title='Histórico de exercícios'/>
+      <ScreenHeader title='Histórico de exercícios' />
+      <HistoryCard/>
+      <HistoryCard/>
+      <HistoryCard/>
     </VStack>
   )
 }
