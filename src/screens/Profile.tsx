b20a4401976aa57c623b756a0d76fc906fbc19e1
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2023-04-02 20:15:34 -0300

    chore: finalizando acesso ao album de fotos para alteração de imagem de perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from 'native-base';
+import { Alert, TouchableOpacity } from 'react-native';
+import { Center, Heading, ScrollView, Skeleton, Text, VStack, useToast } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -13,6 +14,9 @@ const PHOTO_SIZE = 33;
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState('http://github.com/pedrocmoreira.png');
+
+  const toast = useToast();
+  
 
   async function handleUserPhotoSelect() {
     try {
@@ -29,6 +33,17 @@ export function Profile() {
       }
 
       if(photoSelected.assets[0].uri){
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+        
+        if(photoInfo.size && (photoInfo.size / 1024 / 1024 ) > 5){
+         return toast.show({
+           title: 'Essa imagem é muito grande. Escolha uma de até 5MB',
+           placement: 'top',
+           bgColor: 'red.500'
+         })
+         
+        }
+
         setUserPhoto(photoSelected.assets[0].uri);
       }
     } catch (error) {
