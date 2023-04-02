ad065b27ad5a2cf749d7c018631c9b8bf8b382ca
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-04-02 13:03:37 -0300

    fix: ajustes finais nas interfaces

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,10 +1,10 @@
 import { Heading, HStack, Text, VStack } from "native-base";
 
-export function HistoryCard(){
-  return(
-    <HStack w='full' px={5} py={4} mb={3} bg='gray.600' rounded='md' alignItems='center'  justifyContent='space-between'>
-      <VStack mr={5}>
-        <Heading color='white' fontSize='md' textTransform='capitalize'>
+export function HistoryCard() {
+  return (
+    <HStack w='full' px={5} py={4} mb={3} bg='gray.600' rounded='md' alignItems='center' justifyContent='space-between'>
+      <VStack mr={5} flex={1}>
+        <Heading color='white' fontSize='md' textTransform='capitalize' numberOfLines={1}>
           Costas
         </Heading>
 
@@ -12,10 +12,11 @@ export function HistoryCard(){
           PuxadaFrontal
         </Text>
 
-        <Text color='gray.300' fontSize='md'>
-          08:59
-        </Text>
       </VStack>
+
+      <Text color='gray.300' fontSize='md'>
+        08:59
+      </Text>
     </HStack>
   )
 }
--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,6 +1,6 @@
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Icon, HStack, Heading, Text, Image, Box } from 'native-base';
+import { VStack, Icon, HStack, Heading, Text, Image, Box, ScrollView } from 'native-base';
 
 import { Feather } from '@expo/vector-icons';
 
@@ -20,56 +20,59 @@ export function Exercise() {
 
   return (
     <VStack flex={1}>
-      <VStack px={8} bg='gray.600' pt={12}>
-        <TouchableOpacity onPress={handleGoBack}>
-          <Icon as={Feather} name='arrow-left' color='green.500' size={6} />
-        </TouchableOpacity>
 
-        <HStack justifyContent='space-between' mt={4} mb={8} alignItems='center' >
-          <Heading color='gray.100' fontSize='lg' flexShrink={1}>
-            Puxada Frontal
-          </Heading>
+        <VStack px={8} bg='gray.600' pt={12}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Icon as={Feather} name='arrow-left' color='green.500' size={6} />
+          </TouchableOpacity>
 
-          <HStack alignItems='center'>
-            <BodySvg />
-            <Text color='gray.200' ml={1} textTransform='capitalize'>
-              Costas
-            </Text>
-          </HStack>
-        </HStack>
-      </VStack>
+          <HStack justifyContent='space-between' mt={4} mb={8} alignItems='center' >
+            <Heading color='gray.100' fontSize='lg' flexShrink={1}>
+              Puxada Frontal
+            </Heading>
 
-      <VStack p={8}>
-        <Image
-          w='full'
-          h={80}
-          source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
-          alt='Nome do exercício'
-          mb={3}
-          resizeMode='cover'
-          rounded='lg'
-        />
-        <Box bg='gray.400' rounded='md' pb={4} px={4}>
-          <HStack alignItems='center' justifyContent='space-around' mb={5} mt={5}>
-            <HStack>
-              <SeriesSvg />
-              <Text color='gray.200' ml={2}>
-                3 séries
-              </Text>
-            </HStack>
-            <HStack>
-              <RepetitionsSvg />
-              <Text color='gray.200' ml={2}>
-                12 repetições
+            <HStack alignItems='center'>
+              <BodySvg />
+              <Text color='gray.200' ml={1} textTransform='capitalize'>
+                Costas
               </Text>
             </HStack>
           </HStack>
+        </VStack>
 
-          <Button
-            title='Marcar como realizado'
+    <ScrollView>
+        <VStack p={8}>
+          <Image
+            w='full'
+            h={80}
+            source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+            alt='Nome do exercício'
+            mb={3}
+            resizeMode='cover'
+            rounded='lg'
           />
-        </Box>
-      </VStack>
+          <Box bg='gray.400' rounded='md' pb={4} px={4}>
+            <HStack alignItems='center' justifyContent='space-around' mb={5} mt={5}>
+              <HStack>
+                <SeriesSvg />
+                <Text color='gray.200' ml={2}>
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack>
+                <RepetitionsSvg />
+                <Text color='gray.200' ml={2}>
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button
+              title='Marcar como realizado'
+            />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
